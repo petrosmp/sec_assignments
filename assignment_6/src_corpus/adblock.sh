966cf1e39768227c1dc76bb1cec487d7bbfca003
petrosmp <petrakis798@gmail.com>
2022-12-24 21:54:05 +0200

    Implemented domain name resolution ('-domains' option)

--- assignment_6/src_corpus/adblock.sh
+++ assignment_6/src_corpus/adblock.sh
@@ -6,6 +6,13 @@ IPAddressesSame="IPAddressesSame.txt"
 IPAddressesDifferent="IPAddressesDifferent.txt"
 adblockRules="adblockRules"
 
+function resolve_DNS() {
+    # pass nslookup's output to awk to only keep the IPs
+    # associated with the given domain name
+    # dig +short $i | grep -v ";"
+    nslookup $1 | awk '/^Name:/ {c=2} !--c {print $2}'
+}
+
 function adBlock() {
     if [ "$EUID" -ne 0 ];then
         printf "Please run as root.\n"
@@ -15,8 +22,36 @@ function adBlock() {
         # Find different and same domains in ‘domainNames.txt’ and ‘domainsNames2.txt’ files 
 	# and write them in “IPAddressesDifferent.txt and IPAddressesSame.txt" respectively
         # Write your code here...
-        # ...
-        # ...
+
+        # remove old files if they exist
+        rm ${IPAddressesSame}  -f
+        rm ${IPAddressesDifferent}  -f
+
+        # get the domains that only appear in file1/file2 and combine them
+        only1=$(comm <(sort "${domainNames}") <(sort "${domainNames2}") -2 -3)  # comm needs the files to be sorted
+        only2=$(comm <(sort "${domainNames}") <(sort "${domainNames2}") -1 -3)  # comm needs the files to be sorted
+        diff=("${only1[@]}" "${only2[@]}") # concatenate
+        
+        # get the domains that appear in both files
+        same=$(comm <(sort "${domainNames}") <(sort "${domainNames2}") -1 -2)   # comm needs the files to be sorted
+
+        c=1
+
+        # for each domain, resolve its IP and write it to the file
+        for i in ${same[@]}; do
+            resolve_DNS $i >> ${IPAddressesSame} &
+            printf "started job %s (pid: %s) (%s) \n" "$c" "$!" "$i"
+            c=$(($c+1))
+        done
+        
+        for i in ${diff[@]}; do
+            resolve_DNS $i >> ${IPAddressesDifferent} &
+            printf "started job %s (pid: %s) (%s) \n" "$c" "$!" "$i"
+            c=$(($c+1))
+        done
+        
+        wait
+        echo "Done!"
         true
             
     elif [ "$1" = "-ipssame"  ]; then
